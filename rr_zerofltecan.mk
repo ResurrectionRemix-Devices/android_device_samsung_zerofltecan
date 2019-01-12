d34b915c95751a664a5ec5b0c5e4cb0d25672a45
enesuzun2002 <enesuzun200227@gmail.com>
2019-01-12 09:06:45 +0300

    zerofltecan: RRify

--- rr_zerofltecan.mk
+++ rr_zerofltecan.mk
@@ -1,16 +1,12 @@
 # Initialise device config
 $(call inherit-product, device/samsung/zerofltecan/full_zerofltecan.mk)
 
-
-# Enhanced NFC
-# $(call inherit-product, vendor/lineage/config/nfc_enhanced.mk)
-
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ResurrectionRemix stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="zerofltecan" \
     TARGET_DEVICE="zeroflte"
 
-PRODUCT_NAME := lineage_zerofltecan
+PRODUCT_NAME := rr_zerofltecan
 PRODUCT_DEVICE := zerofltecan
